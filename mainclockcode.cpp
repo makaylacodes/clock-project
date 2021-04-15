ff029b7cc48acb18691bc636c61ca46e444d5aae
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-04-16 02:19:24 +0900

    New Branch
    I have added a new branch named Debugging. The program outputs both time formats, displays the menu options, and loops with user input. The main problem is that the seconds are getting printed twice. Also that the user input is recognized when 1-3 is entered, but it does not display the correct output. It only displays the current time. However, the program does recognize when the user enters 4 or a digit not 1-3 and quits; which is good.

--- mainclockcode.cpp
+++ mainclockcode.cpp
@@ -8,22 +8,32 @@ Assignment: 3-1 Project 1
 #include <ctime>
 #include <time.h>
 
-void display12hours();
-void display24hours();
+
+int addOneHour(int &timeHour, int &timeMinute, int &timeSecond);
+int addOneMinute(int &timeHour, int &timeMinute, int &timeSecond);
+int addOneSecond(int &timeHour, int &timeMinute, int &timeSecond);
+int display12hours(int &timeHour, int &timeMinute, int &timeSecond);
+int display24hours(int &timeHour, int &timeMinute, int &timeSecond);
 
 int main() {
+
+	int timeHour;
+	int timeMinute;
+	int timeSecond;
+	int userChoice;
+	
+	
 
 	// This text will display a 12 hour clock and a 23 hour clock.
 	std::cout << "\t*************************         *************************\n";
 	std::cout << "\t      12 hour clock                      24 hour clock\n";
 	std::cout << "\t       ";
-	display12hours();
+	std::cout << display12hours(timeHour, timeMinute, timeSecond);
 	std::cout << "\t                    ";
-	display24hours();
+	std::cout << display24hours(timeHour, timeMinute, timeSecond);
 	std::cout << " \n";
 	std::cout << "\t*************************         *************************\n\n";
 
-	// This menu allows the user to select their preferred option
 	std::cout << "\t\t\tWhat option would you like?\n";
 	std::cout << "\t\t\t  *********************\n";
 	std::cout << "\t\t\t  * 1. Add one hour   *\n";
@@ -31,48 +41,124 @@ int main() {
 	std::cout << "\t\t\t  * 3. Add one second *\n";
 	std::cout << "\t\t\t  * 4. Exit           *\n";
 	std::cout << "\t\t\t  *********************\n\n";
+	std::cin >> userChoice;
+	int newHour;
+	int newMinute;
+	int newSecond;
+	while (userChoice >= 1 && userChoice <= 3) {
+	// This menu allows the user to select their preferred option
+	
+		if (userChoice == 1) {
+			newHour = addOneHour(timeHour, timeMinute, timeSecond);
+			std::cout << newHour;
+		}
+		else if (userChoice == 2) {
+			newMinute = addOneMinute(timeHour, timeMinute, timeSecond);
+			std::cout << newMinute;
+		}
+		else if(userChoice == 3){
+			newSecond = addOneSecond(timeHour, timeMinute, timeSecond);
+			std::cout << newSecond;
+		}
+		
+	
+			std::cout << "\t\t\tWhat option would you like?\n";
+			std::cout << "\t\t\t  *********************\n";
+			std::cout << "\t\t\t  * 1. Add one hour   *\n";
+			std::cout << "\t\t\t  * 2. Add one minute *\n";
+			std::cout << "\t\t\t  * 3. Add one second *\n";
+			std::cout << "\t\t\t  * 4. Exit           *\n";
+			std::cout << "\t\t\t  *********************\n\n";
+			std::cin >> userChoice;
+		
+	}
 
+	std::cout << "The program has ended.\n";
 
 	return 0;
 }
 
-void display12hours() {
+int addOneHour(int &timeHour, int &timeMinute, int &timeSecond) {
+	timeHour = timeHour + 1;
+	std::cout << "\t*************************         *************************\n";
+	std::cout << "\t      12 hour clock                      24 hour clock\n";
+	std::cout << "\t       ";
+	display12hours(timeHour, timeMinute, timeSecond);
+	std::cout << "\t                    ";
+	display24hours(timeHour, timeMinute, timeSecond);
+	std::cout << " \n";
+	std::cout << "\t*************************         *************************\n\n";
+	return timeHour, timeMinute, timeSecond;
+}
+
+int addOneMinute(int &timeHour, int &timeMinute, int &timeSecond) {
+	timeMinute = timeMinute + 1;
+	std::cout << "\t*************************         *************************\n";
+	std::cout << "\t      12 hour clock                      24 hour clock\n";
+	std::cout << "\t       ";
+	display12hours(timeHour, timeMinute, timeSecond);
+	std::cout << "\t                    ";
+	display24hours(timeHour, timeMinute, timeSecond);
+	std::cout << " \n";
+	std::cout << "\t*************************         *************************\n\n";
+
+	return timeHour, timeMinute, timeSecond;
+}
+
+int addOneSecond(int &timeHour, int &timeMinute, int &timeSecond) {
+	timeSecond = timeSecond + 1;
+	std::cout << "\t*************************         *************************\n";
+	std::cout << "\t      12 hour clock                      24 hour clock\n";
+	std::cout << "\t       ";
+	display12hours(timeHour, timeMinute, timeSecond);
+	std::cout << "\t                    ";
+	display24hours(timeHour, timeMinute, timeSecond);
+	std::cout << " \n";
+	std::cout << "\t*************************         *************************\n\n";
+
+	return timeHour, timeMinute, timeSecond;
+}
+
+int display12hours(int &timeHour, int &timeMinute, int &timeSecond) {
 
 	struct tm tempLocalTime;
 	time_t localTime = time(0);
 	localtime_s(&tempLocalTime, &localTime);
-	int timeHour = tempLocalTime.tm_hour;
-	int timeMinute = tempLocalTime.tm_min;
-	int timeSeconds = tempLocalTime.tm_sec;
-	int t12;
-	int t12AM;
+	timeHour = tempLocalTime.tm_hour;
+	timeMinute = tempLocalTime.tm_min;
+	timeSecond = tempLocalTime.tm_sec;
+	
 	if (timeHour == 0) {
-		t12AM = 12;
-		std::cout << t12AM << ":" << timeMinute << ":" << timeSeconds << " AM";
+		timeHour = 12;
+		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " AM ";
 
 	}
 
 	else if (timeHour > 12) {
-		t12 = timeHour - 12;
-		std::cout << t12 << ":" << timeMinute << ":" << timeSeconds << " PM";
+		timeHour -= 12;
+		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " PM ";
 
 	}
+
 	else {
-		std::cout << timeHour << ":" << timeMinute << ":" << timeSeconds << " AM";
+		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " AM";
 	}
+	return timeHour, timeMinute, timeSecond;
+
 	//be sure to run this program in the morning when it is still signle digit to be
 	//sure that it works properly
 }
-void display24hours() {
+
+int display24hours(int &timeHour, int &timeMinute, int &timeSecond) {
 
 	struct tm tempLocalTime;
 	time_t localTime = time(0);
 	localtime_s(&tempLocalTime, &localTime);
-	int timeHour = tempLocalTime.tm_hour;
-	int timeMinute = tempLocalTime.tm_min;
-	int timeSeconds = tempLocalTime.tm_sec;
-	std::cout << timeHour << ":" << timeMinute << ":" << timeSeconds;
+	timeHour = tempLocalTime.tm_hour;
+	timeMinute = tempLocalTime.tm_min;
+	timeSecond = tempLocalTime.tm_sec;
+	std::cout << timeHour << ":" << timeMinute << ":" << timeSecond;
 
-
+	return timeHour, timeMinute, timeSecond;
 	
 }
