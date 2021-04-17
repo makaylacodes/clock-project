9cf586f807c6087887186053f5b942123fb52c12
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-04-17 15:05:54 +0900

    All bugs fixed
    All bugs have been fixed. The time prints properly and correctly recognizes user input. To fix the bug, I put the time struct in the main instead of in a separate function! I also made some minor changes so that the 12 hour clock only prints 12 hours and the same for the 24 hour clock. My final commit will include comments in the code.

--- Debugging-File.cpp
+++ Debugging-File.cpp
@@ -3,20 +3,32 @@
 #include <ctime>
 #include <time.h>
 
-int addOneHour(int&, int& , int& ); // This function will add 1 hour to the time
-int addOneMinute(int& , int&, int&); // This function will add 1 minute to the time
+
+
+void addOneHour(int & , int&, int&); // This function will add 1 hour to the time
+int addOneMinute(int&, int&, int&); // This function will add 1 minute to the time
 int addOneSecond(int&, int&, int&); // This function will add one second to the time
-void display12hours(int&, int&, int& ); // This function will display the time in a 12 hour clock format
-void display24hours(int&, int& , int&); // This function will display the time in a 24 hour clock format
+void display12hours(int&, int&, int&); // This function will display the time in a 12 hour clock format
+void display24hours(int&, int&, int&); // This function will display the time in a 24 hour clock format
+
+
 
 int main() {
-
 	int timeHour; // this variable represents the current hour in time
 	int timeMinute; // this variable represents the current minute in time
 	int timeSecond; // this variable represents the current second in time
-	int userChoice; // this variable will read user input for the menu options
 
+
+	int userChoice; // this variable will read user input for the menu options
 	// This text will display a 12 hour clock and a 24 hour clock.
+
+	struct tm tempLocalTime;
+	time_t localTime = time(0);
+	localtime_s(&tempLocalTime, &localTime);
+	timeHour = tempLocalTime.tm_hour; // This variable is storing the current hour
+	timeMinute = tempLocalTime.tm_min; // This variable is storing the current minute
+	timeSecond = tempLocalTime.tm_sec; // this variable is storing the current second
+
 	std::cout << "\t*************************         *************************\n";
 	std::cout << "\t      12 hour clock                      24 hour clock\n";
 	std::cout << "\t       ";
@@ -26,6 +38,7 @@ int main() {
 	std::cout << " \n";
 	std::cout << "\t*************************         *************************\n\n";
 
+	
 	// This text will display a menu with options for altering the time. The user will input their selection
 	std::cout << "\t\t\tWhat option would you like?\n";
 	std::cout << "\t\t\t  *********************\n";
@@ -44,6 +57,7 @@ int main() {
 		
 	// if user chooses 1, 1 will be added to the hour variable and both clock formats will be displayed
 		if (userChoice == 1) {
+			
 			addOneHour(timeHour, timeMinute, timeSecond);
 			
 		}
@@ -79,8 +93,8 @@ int main() {
 }
 
 // This function will add 1 to the hour variable and display the updated time format.
-int addOneHour(int &timeHour, int &timeMinute, int &timeSecond) {
-	timeHour += 1;
+void addOneHour(int &timeHour,int &timeMinute,int &timeSecond) {
+	timeHour = timeHour + 1;
 	std::cout << "\t*************************         *************************\n";
 	std::cout << "\t      12 hour clock                      24 hour clock\n";
 	std::cout << "\t       ";
@@ -89,11 +103,12 @@ int addOneHour(int &timeHour, int &timeMinute, int &timeSecond) {
 	display24hours(timeHour, timeMinute, timeSecond);
 	std::cout << " \n";
 	std::cout << "\t*************************         *************************\n\n";
-	return timeHour;
+	
 }
 
 // This function will add 1 to the minute variable and display the updated time format.
-int addOneMinute(int &timeHour, int &timeMinute, int &timeSecond) {
+int addOneMinute(int& timeHour, int& timeMinute, int& timeSecond) {
+	
 	timeMinute = timeMinute + 1;
 	std::cout << "\t*************************         *************************\n";
 	std::cout << "\t      12 hour clock                      24 hour clock\n";
@@ -109,7 +124,9 @@ int addOneMinute(int &timeHour, int &timeMinute, int &timeSecond) {
 
 
 // This function will add 1 to the second variable and display the updated time format.
-int addOneSecond(int &timeHour, int &timeMinute, int &timeSecond) {
+int addOneSecond(int& timeHour, int& timeMinute, int& timeSecond) {
+
+	
 	timeSecond = timeSecond + 1;
 	std::cout << "\t*************************         *************************\n";
 	std::cout << "\t      12 hour clock                      24 hour clock\n";
@@ -126,27 +143,31 @@ int addOneSecond(int &timeHour, int &timeMinute, int &timeSecond) {
 
 
 // This function will display the time in a 12 hour clock format
-void display12hours(int& timeHour, int& timeMinute, int& timeSecond) {
+void display12hours(int &timeHour, int &timeMinute, int &timeSecond) {
 
-	struct tm tempLocalTime;
-	time_t localTime = time(0);
-	localtime_s(&tempLocalTime, &localTime);
-	timeHour = tempLocalTime.tm_hour;
-	timeMinute = tempLocalTime.tm_min;
-	timeSecond = tempLocalTime.tm_sec;
-
-	//This if statement will covert 00:00 in 24 hour format to 12 AM for the 12 hour format
-	if (timeHour == 0) {
-		timeHour = 12;
+	if (timeHour > 24) {
+		timeHour = 0;
+	}
+	if (timeMinute > 59) {
+		timeMinute = 0;
+	}
+	if (timeSecond > 59) {
+		timeSecond = 0;
+	}
+	if (timeHour == 24)
+	{
+		timeHour = 0;
 		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " AM ";
 
 	}
+	
+	
 
 	//This else if statement will convert the 24 hour format to the 12 hour format, if the 
 	// hour is greater than 12
 	else if (timeHour > 12) {
-		timeHour -= 12;
-		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " PM ";
+		
+		std::cout << timeHour - 12 << ":" << timeMinute << ":" << timeSecond << " PM ";
 
 	}
 
@@ -155,25 +176,28 @@ void display12hours(int& timeHour, int& timeMinute, int& timeSecond) {
 		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " PM ";
 
 	}
-
+	
 	//This else statement will display the time in a 12 hour format because the hour is 
 	// less than 12
-	else {
+	else if (timeHour >= 1 && timeHour < 12) {
 		std::cout << timeHour << ":" << timeMinute << ":" << timeSecond << " AM";
 	}
+	
 }
 
 
 // This function will display the time in a 24 hour clock format
-void display24hours(int& timeHour, int& timeMinute, int& timeSecond) {
+void display24hours(int &timeHour, int &timeMinute, int &timeSecond) {
 
-	struct tm tempLocalTime;
-	time_t localTime = time(0);
-	localtime_s(&tempLocalTime, &localTime);
-	timeHour = tempLocalTime.tm_hour; // This variable is storing the current hour
-	timeMinute = tempLocalTime.tm_min; // This variable is storing the current minute
-	timeSecond = tempLocalTime.tm_sec; // this variable is storing the current second
-
+	if (timeHour > 24) {
+		timeHour = 0;
+	}
+	if (timeMinute > 59) {
+		timeMinute = 0;
+	}
+	if (timeSecond > 59) {
+		timeSecond = 0;
+	}
 	std::cout << timeHour << ":" << timeMinute << ":" << timeSecond; // This statement will display the time in a 24 hour format
 
 }
